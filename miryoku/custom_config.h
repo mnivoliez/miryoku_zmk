4deb4cc93870162d3b1a0eb5232625588d257744
mnivoliez <mnivoliez@gmail.com>
2024-10-17 21:09:28 +0200

    Update custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -18,7 +18,7 @@ U_NP,              U_NP,              &to U_BASE,        &kp SPC,           &non
 
 #if defined (MIRYOKU_KEYBOARD_EZBOARD)
 
-#define MIRYOKU_LAYOUTMAPPING_BASE( \
+#define MIRYOKU_LAYERMAPPING_BASE( \
      K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
      K10, K11, K12, K13, K14,      K15, K16, K17, K18, K19, \
      K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
@@ -29,7 +29,7 @@ XXX  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  XXX \
 XXX  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  XXX \
                K32  K33  K34       K35  K36  K37
 
-#define MIRYOKU_LAYOUTMAPPING_GAME( \
+#define MIRYOKU_LAYERMAPPING_GAME( \
      K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
      K10, K11, K12, K13, K14,      K15, K16, K17, K18, K19, \
      K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
